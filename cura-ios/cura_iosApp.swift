6d385e75c43f3ff9e87056f6cf37388d5c3df27d
Thomas Vignos <tjv37@cornell.edu>
2024-01-19 11:42:00 -0500

    Initial Commit

---
+++ cura-ios/cura_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  cura_iosApp.swift
+//  cura-ios
+//
+//  Created by thomas vignos on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct cura_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
